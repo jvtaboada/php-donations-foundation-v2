ba4fed154af8e6d41199728525969e1f7069dbdb
João Vitor <joao_taboada@hotmail.com>
2022-11-17 22:48:46 -0300

    Fix/Feat: finalizada o estilo e configuração da página

--- api/catalogo_instituicoes/catalogo.php
+++ api/catalogo_instituicoes/catalogo.php
@@ -4,26 +4,27 @@
 ?>
 
 <!DOCTYPE html>
-    <html lang="en">
+    <html lang="pt-BR">
         <head>
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
             <link href="/style/reset.css" rel="stylesheet"> 
-            <link href="/style/instituicoes.css" rel="stylesheet">
+            <link href="/style/catalogo.css" rel="stylesheet">
             <link href="/style/footer.css" rel="stylesheet">
-
+            
             <link rel = "shortcut icon" type = "imagem/x-icon" href = "/img/instituicao-ico.png"/>
             <title>Catálogo de Instituições</title>
         </head>
 
-        <body>  
-            <h1>Titulo da div</h1>
+        <body class="catalogo-body">  
+            <div class="titles">
+                <h1 class="inst-title">Catálogo de Instituições parceiras</h1>
+                <h2 class="inst-subtitle">Conheça as instituições parceiras do projeto</h2>
+            </div>
             
             <div class="card-wrapper">
-                
-
                 <?php
                     $sql = "SELECT * 
                             FROM instituicoes
@@ -32,9 +33,7 @@
 
                     while ($linha = mysqli_fetch_array($tabela))
                     {
-                ?>
-                    
-                    
+                ?>  
                 <div class="card-inst">
                     <h2 class="nome-inst"><?php echo $linha["nome"]; ?></h2>
 
@@ -48,5 +47,21 @@
                 ?>	
 
             </div>
+
+            <footer>
+            <div class="footer-line"></div>
+
+            <div class="social-icon-wrapper">
+                <img class="social-icon" src="/img/insta-icon.webp">
+                <img class="social-icon" src="/img/face-icon.png">
+                <img class="social-icon" src="/img/linkedin-icon.png">
+                <img class="social-icon" src="/img/ghub-icon.webp">
+            </div>
+
+            <span class="foot-text">
+                © PHP Donations Foundation Inc 2022. <br>All Rights Reserved.
+            </span>
+            
+        </footer>
         </body>
     </html>
